744eff08d4b72de9d25679c1873ca0c473469e54
martin-creator <martinlubowa@outlook.com>
2023-11-30 18:06:40 +0300

    Created basic client

--- pyclient/basic.py
+++ pyclient/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/status/200"
 # endpoint = "https://httpbin.org/anything"
 
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api/"
 
 get_response =  requests.get(endpoint) # GET
 print(get_response.text)
