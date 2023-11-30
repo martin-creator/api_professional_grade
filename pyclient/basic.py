ec49343dc6c3788efe86b9b514e767b78f5e3a95
martin-creator <martinlubowa@outlook.com>
2023-11-30 17:09:15 +0300

    Created new api home

---
+++ pyclient/basic.py
@@ -0,0 +1,10 @@
+import requests
+
+
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+
+endpoint = "http://127.0.0.1:8000/"
+
+get_response =  requests.get(endpoint) # GET
+print(get_response.text)
