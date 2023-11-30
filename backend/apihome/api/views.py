ceedbd62d7b720cc0a0b67e942acd062512ebaf6
martin-creator <martinlubowa@outlook.com>
2023-11-30 18:06:28 +0300

    implemented logic for view

--- backend/apihome/api/views.py
+++ backend/apihome/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def api_home(request, *args, **kwargs):
+    body = request.body
+    data = {}
+
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    
+    print(data)
+    data['headers'] = request.headers
+    return JsonResponse(data)
 
 # Create your views here.
